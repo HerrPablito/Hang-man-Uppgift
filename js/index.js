b72a1405fefb7c695ee8c9ae43e93d131ff8ebde
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2022-10-31 15:15:55 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,33 @@
 
 
+let hangmanNumber = 0;
+hangmanNumber = hangmanNumber++
+let bodyArr;
 
 
-// let bodyArr = [
-//  ,
-//  document.querySelector('figure').classList.add('head'),
-//  document.querySelector('figure').classList.add('body'),
-//  document.querySelector('figure').classList.add('arms'),
-//  document.querySelector('figure').classList.add('legs')
-
-// ]
-
-
-let guessLetter = 'a';
+let guessLetter = 'b';
 let finalWord = 'a'
 
+function arrangeBody(){
+  
+ bodyArr = [
+ document.querySelector('figure').classList.add('scaffold'),
+ document.querySelector('figure').classList.add('head'),
+ document.querySelector('figure').classList.add('body'),
+ document.querySelector('figure').classList.add('arms'),
+ document.querySelector('figure').classList.add('legs')
+]
+}
 
 function showBody(let, ord){
   if (let != ord){
-    document.querySelector('figure').classList.add('scaffold')
-  //  for (let i = 0; i < bodyArr.length; i++) {
-  // let getbBodyParts = bodyArr[3];
-  // return getbBodyParts;
+    arrangeBody()
+    
    }
   if (let == ord){
     console.log(guessLetter);
   } }  
  
+
 
 showBody(guessLetter, finalWord)
