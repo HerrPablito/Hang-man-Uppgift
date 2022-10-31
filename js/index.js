8d3fe62dad32e8d42521fd0024c22d545abba153
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2022-10-31 11:55:56 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
 
 
-function show () {
-  
+
+let bodyArr = [
  document.querySelector('figure').classList.add('scaffold'),
  document.querySelector('figure').classList.add('head'),
  document.querySelector('figure').classList.add('body'),
  document.querySelector('figure').classList.add('arms'),
  document.querySelector('figure').classList.add('legs')
+];
+  for (let i = 0; i < bodyArr.length; i++) {
+      const bodyParts = bodyArr[i];
+     console.log(bodyParts); 
+  };
+ 
 
-  
-}
+
+function showBodyParts () {
 
 
-show()
+
+};
+
+showBodyParts()
+
+
+
