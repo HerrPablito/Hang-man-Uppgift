c6e3c3e3e68b8337037992c5df7ee07dd04987aa
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2022-10-31 22:58:02 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,36 +1,74 @@
 
 
 
-let bodyArr;
+// let bodyArr;
+ let guessLetter = 'a';
+ let finalWord = 'b'
+ let numberOfGuesses = 5;  
 
 
-let guessLetter = 'b';
-let finalWord = 'a'
+// function showBody(hej, ord){
+//   let youDead = arrangeBody();
+//   let hangmanNumber = 0;
+//   hangmanNumber = hangmanNumber++;
 
+//   if (hej != ord){
+//     youDead[4]
+//    }
+//   else if (hej == ord){
+//     console.log(hej);
+//   } 
+//   }  
 
+//document.querySelector('figure').classList.add('scaffold');
+//let head = document.querySelector('figure').classList.add('head');
+//let bod = document.querySelector('figure').classList.add('body');
+//let arm = document.querySelector('figure').classList.add('arms');
+//let leg =document.querySelector('figure').classList.add('legs');
+//  bodyArr = [sca, head, bod, arm, leg ]
+//  console.log(bodyArr); 
+// function arrangeBody(){
+// }
+// arrangeBody()
 
-function showBody(let, ord){
-  let youDead = arrangeBody();
-  let hangmanNumber = 0;
-  hangmanNumber = hangmanNumber++;
-  if (let != ord){
-    youDead[hangmanNumber]
-    
-   }
-  else if (let == ord){
-    console.log(guessLetter);
-  } 
-  }  
- 
-function arrangeBody(){
-  
- bodyArr = [
- document.querySelector('figure').classList.add('scaffold'),
- document.querySelector('figure').classList.add('head'),
- document.querySelector('figure').classList.add('body'),
- document.querySelector('figure').classList.add('arms'),
- document.querySelector('figure').classList.add('legs')
-]
+//showBody(guessLetter, finalWord)
+
+function test (guessLetter, finalWord){
+  if (guessLetter =! finalWord){
+    numberOfGuesses = numberOfGuesses++
+    if (numberOfGuesses == 1)
+    document.querySelector('figure').classList.add('scaffold');
+    else if (numberOfGuesses == 2){
+      document.querySelector('figure').classList.add('scaffold');
+      document.querySelector('figure').classList.add('head');
+    }
+
+    else if (numberOfGuesses == 3){
+      document.querySelector('figure').classList.add('scaffold');
+      document.querySelector('figure').classList.add('head');
+      document.querySelector('figure').classList.add('body');
+    }
+
+    else if (numberOfGuesses == 4){
+      document.querySelector('figure').classList.add('scaffold');
+      document.querySelector('figure').classList.add('head');
+      document.querySelector('figure').classList.add('body');
+      document.querySelector('figure').classList.add('arms');
+    }
+
+    else if (numberOfGuesses == 5){
+      document.querySelector('figure').classList.add('scaffold');
+      document.querySelector('figure').classList.add('head');
+      document.querySelector('figure').classList.add('body');
+      document.querySelector('figure').classList.add('arms');
+      document.querySelector('figure').classList.add('legs');
+      console.log("du förlorade");
+    }
+
+    else {
+     console.log("spela igen?");
+    }
+};
 }
+test();
 
-showBody(guessLetter, finalWord)
