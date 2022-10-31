a60ddd3752acae2f7d2ac5d17945b0d3111c3534
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2022-10-31 14:52:40 +0100

    update js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 
- document.querySelector('figure').classList.add('scaffold'),
- document.querySelector('figure').classList.add('head'),
- document.querySelector('figure').classList.add('body'),
- document.querySelector('figure').classList.add('arms'),
- document.querySelector('figure').classList.add('legs')
 
 
 
+// let bodyArr = [
+//  ,
+//  document.querySelector('figure').classList.add('head'),
+//  document.querySelector('figure').classList.add('body'),
+//  document.querySelector('figure').classList.add('arms'),
+//  document.querySelector('figure').classList.add('legs')
+
+// ]
 
 
+let guessLetter = 'a';
+let finalWord = 'a'
 
 
+function showBody(let, ord){
+  if (let != ord){
+    document.querySelector('figure').classList.add('scaffold')
+  //  for (let i = 0; i < bodyArr.length; i++) {
+  // let getbBodyParts = bodyArr[3];
+  // return getbBodyParts;
+   }
+  if (let == ord){
+    console.log(guessLetter);
+  } }  
+ 
 
-
+showBody(guessLetter, finalWord)
