d9469964d90cf4e1bbe5d1f1bd2d92227c4a7961
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2022-10-31 13:14:59 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,31 +1,11 @@
 
-
-
-
- 
-function showBodyParts () {
- let bodyArr = [
  document.querySelector('figure').classList.add('scaffold'),
  document.querySelector('figure').classList.add('head'),
  document.querySelector('figure').classList.add('body'),
  document.querySelector('figure').classList.add('arms'),
  document.querySelector('figure').classList.add('legs')
-];
-  for (let i = 0; i < bodyArr.length; i++) {
-     // const bodyParts = bodyArr[i];
-     console.log(bodyArr[i])
-  };   
-  
-
-};
 
 
-window.addEventListener('keydown' , (event) =>{
-    if (event.code  === "Arrowleft"){
-        showBodyParts(bodyArr[i])
-    }
-
-});
 
 
 
