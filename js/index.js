cb8f8303e9fe3c7e607b889e91f3b080ba73ef07
simeedin <113066728+simeedin@users.noreply.github.com>
2022-10-31 21:29:18 +0100

    Update index.js
    new function

--- js/index.js
+++ js/index.js
@@ -34,3 +34,39 @@ function arrangeBody(){
 }
 
 showBody(guessLetter, finalWord)
+
+
+/**
+ function generateWord() {
+    let wordArr = ['summer', 'winter', 'spring', 'fall'];
+    const randomPosition = Math.floor(Math.random() * wordArr.length);
+    const pickedWord = wordArr.splice(randomPosition, 1);
+    console.log(pickedWord);
+
+    return pickedWord;
+
+}
+
+generateWord();
+
+
+
+function checkLetter(word, letter) {
+    const getLetter = word.includes(letter);
+
+    return getLetter;
+
+}
+
+function showLetter(getLetter) {
+    console.log(getLetter);
+}
+
+function start() {
+    const letterResault = checkLetter(pickedWord, 'e');
+
+    showLetter(letterResault);
+}
+
+start();
+ */
